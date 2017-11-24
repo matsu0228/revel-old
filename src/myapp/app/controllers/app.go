145fbec406d413177e40773be9e17b0018c520fa
matsu ki <m.hisa3153@gmail.com>
2017-11-25 05:05:34 +0900

    connect mariaDB

--- src/myapp/app/controllers/app.go
+++ src/myapp/app/controllers/app.go
@@ -1,16 +1,99 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/revel/revel"
+	"strings"
+	// "myapp/app/models/db" //not work
 )
 
 type App struct {
 	*revel.Controller
 }
 
+func getConfigParam(param string, defaultValue string) string {
+	p, found := revel.Config.String(param)
+	if !found {
+		if defaultValue == "" {
+			revel.ERROR.Fatal("Cound not find parameter: " + param)
+		} else {
+			return defaultValue
+		}
+	}
+	return p
+}
+
+func getConnectionString() string {
+	host := getConfigParam("db.host", "")
+	port := getConfigParam("db.port", "3306")
+	user := getConfigParam("db.user", "")
+	pass := getConfigParam("db.password", "")
+	dbname := getConfigParam("db.name", "")
+	protocol := getConfigParam("db.protocol", "tcp")
+	dbargs := getConfigParam("db.args", " ")
+	if strings.Trim(dbargs, " ") != "" {
+		dbargs = "?" + dbargs
+	} else {
+		dbargs = ""
+	}
+	return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s%s",
+		user, pass, protocol, host, port, dbname, dbargs)
+}
+
+func gormConnect() *gorm.DB {
+	connectionString := getConnectionString()
+	db, err := gorm.Open("mysql", connectionString)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+type Book struct {
+	Id     int    `json:id`
+	Name   string `json:name`
+	Author string `json:author`
+}
+
+func getBooks() string {
+	db := gormConnect()
+	// var book Book
+	var count = 0
+	var output = ""
+
+	// insert
+	// ---------------------------
+	new_book := Book{Name: "Sikisai", Author: "Haruki"}
+	// if db.NewRecord(new_book) // => returns `true` as primary key is blank
+	db.Create(&new_book)
+
+	// select
+	// ---------------------------
+	bookEx := []Book{}
+	// bookEx.Id = 1
+	db.Find(&bookEx).Count(&count)
+	// db.Last(&bookEx).Count(&count)
+	// db.First(&bookEx).Count(&count)
+	if count == 0 {
+		fmt.Println("該当レコードなし")
+	} else {
+		for i := 0; i < len(bookEx); i++ {
+			output += "\nbook:" + fmt.Sprint(bookEx[i].Id) + " =id/ " + bookEx[i].Name + " / " + bookEx[i].Author
+		}
+	}
+	defer db.Close()
+	return output
+}
+
+// ----------------------------------------------------------------------------
 func (c App) Index() revel.Result {
 	// return c.Render()
-	greeting := "Aloha World"
+	// connectionString := getConnectionString()
+	book_data := getBooks()
+	greeting := "Aloha World/" + book_data
 	return c.Render(greeting)
 }
 func (c App) Hello(myName string) revel.Result {
--- src/myapp/app/models/db/init.go
+++ src/myapp/app/models/db/init.go
@@ -1,26 +1,31 @@
-// package db
+// package dbinit
 package main
 
 import (
 	"fmt"
-	"github.com/jinzhu/gorm"
+	// "github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/revel/revel"
+	// "strings"
 	// "myapp/app/models/entity"
 )
 
-func gormConnect() *gorm.DB {
-	USER := "dev"
-	PASS := "dev1709"
-	PROTOCOL := "tcp(127.0.0.1:3306)"
-	DBNAME := "go_tutorial"
-
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=True&loc=Local"
-	db, err := gorm.Open("mysql", CONNECT)
-
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
+// func gormConnect() *gorm.DB {
+func gormConnect() {
+	// USER := "dev"
+	// PASS := "dev1709"
+	// PROTOCOL := "tcp(127.0.0.1:3306)"
+	// DBNAME := "go_tutorial"
+	// CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=True&loc=Local"
+	connectionString := getConnectionString()
+	// ---------------------------
+	fmt.Println("con:" + connectionString)
+	// db, err := gorm.Open("mysql", connectionString)
+	//
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// return db
 }
 
 type Book struct {
@@ -37,30 +42,63 @@ type Book struct {
 // 	defer db.Close()
 // }
 
-func main() {
-	db := gormConnect()
-	// var book Book
-	var count = 0
-
-	// insert
-	// ---------------------------
-	new_book := Book{Name: "Sikisai", Author: "Haruki"}
-	// if db.NewRecord(new_book) // => returns `true` as primary key is blank
-	db.Create(&new_book)
-
-	// select
-	// ---------------------------
-	bookEx := []Book{}
-	// bookEx.Id = 1
-	db.Find(&bookEx).Count(&count)
-	// db.Last(&bookEx).Count(&count)
-	// db.First(&bookEx).Count(&count)
-	if count == 0 {
-		fmt.Println("該当レコードなし")
-	} else {
-		for i := 0; i < len(bookEx); i++ {
-			fmt.Println("book:" + fmt.Sprint(bookEx[i].Id) + " =id/ " + bookEx[i].Name + " / " + bookEx[i].Author)
+func getConfigParam(param string, defaultValue string) string {
+	p, found := revel.Config.String(param)
+	if !found {
+		if defaultValue == "" {
+			revel.ERROR.Fatal("Cound not find parameter: " + param)
+		} else {
+			return defaultValue
 		}
 	}
-	defer db.Close()
+	return p
+}
+
+func getConnectionString() string {
+	host := getConfigParam("db.host", "")
+	port := getConfigParam("db.port", "3306")
+	user := getConfigParam("db.user", "")
+	pass := getConfigParam("db.password", "")
+	dbname := getConfigParam("db.name", "")
+	protocol := getConfigParam("db.protocol", "tcp")
+	// dbargs := getConfigParam("db.args", " ")
+	//
+	// if strings.Trim(dbargs, " ") != "" {
+	// 	dbargs = "?" + dbargs
+	// } else {
+	// 	dbargs = ""
+	// }
+	dbargs := ""
+	return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s%s",
+		user, pass, protocol, host, port, dbname, dbargs)
+}
+
+func main() {
+	connectionString := getConnectionString()
+	fmt.Println("con:" + connectionString)
+	// db := gormConnect()
+	// // var book Book
+	// var count = 0
+	//
+	// // insert
+	// // ---------------------------
+	// new_book := Book{Name: "Sikisai", Author: "Haruki"}
+	// // if db.NewRecord(new_book) // => returns `true` as primary key is blank
+	// db.Create(&new_book)
+	//
+	// // select
+	// // ---------------------------
+	// bookEx := []Book{}
+	// // bookEx.Id = 1
+	// db.Find(&bookEx).Count(&count)
+	// // db.Last(&bookEx).Count(&count)
+	// // db.First(&bookEx).Count(&count)
+	// if count == 0 {
+	// 	fmt.Println("該当レコードなし")
+	// } else {
+	// 	for i := 0; i < len(bookEx); i++ {
+	// 		fmt.Println("book:" + fmt.Sprint(bookEx[i].Id) + " =id/ " + bookEx[i].Name + " / " + bookEx[i].Author)
+	// 	}
+	// }
+	// defer db.Close()
 }
